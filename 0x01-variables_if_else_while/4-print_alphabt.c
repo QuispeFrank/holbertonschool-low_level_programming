5ef9750e0fe504aef9e05cb7489a7e47d1535420
FranQ <frankchristopherquispe@gmail.com>
2022-02-04 15:56:42 -0800

    0x01 - ADD: 4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101 || i == 113)
+		{
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+		putchar(10);
+
+		return (0);
+}
