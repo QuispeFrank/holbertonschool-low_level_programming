74d2fb9c2d38f8bc24e813ba5f16555ab4385fa5
FranQ <frankchristopherquispe@gmail.com>
2022-02-04 15:12:06 -0800

    0x01 - ADD: 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = '1';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+		putchar(10);
+
+		return (0);
+}
