161c369bc966eaa2039e17e8bc495273006d1de2
FranQ <frankchristopherquispe@gmail.com>
2022-02-04 15:23:40 -0800

    0x01 - ADD: 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+		putchar(10);
+
+		return (0);
+}
