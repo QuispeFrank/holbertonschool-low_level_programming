ddc13ebf28cf7af5f317f7f6cea0e46d2af9cfd6
FranQ <frankchristopherquispe@gmail.com>
2022-02-04 17:06:06 -0800

    0x01 - ADD: 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+		putchar(10);
+
+		return (0);
+}
