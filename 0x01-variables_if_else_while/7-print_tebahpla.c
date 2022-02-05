94a838014b44c7e575b70b0b2bfbdcdbe2adc5d4
FranQ <frankchristopherquispe@gmail.com>
2022-02-04 16:49:25 -0800

    0x01 - ADD: 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+		putchar(10);
+
+		return (0);
+}
