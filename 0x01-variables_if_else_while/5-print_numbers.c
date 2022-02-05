32de3c362b840816b5c9c69dd9b1bdc2e035557c
FranQ <frankchristopherquispe@gmail.com>
2022-02-04 16:36:47 -0800

    0x01 - ADD: 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+		putchar(10);
+
+		return (0);
+}
