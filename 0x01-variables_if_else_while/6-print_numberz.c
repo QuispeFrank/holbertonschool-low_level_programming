16ed3ca6e1313500665df55d105c6bb6e93fb184
FranQ <frankchristopherquispe@gmail.com>
2022-02-04 16:41:34 -0800

    0x01 - ADD: 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+		putchar(10);
+
+		return (0);
+}
