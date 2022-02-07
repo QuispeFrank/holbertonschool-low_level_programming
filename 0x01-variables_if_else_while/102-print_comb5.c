d1fbe7a1b2955580697fb3334440181c447f5a66
FranQ <frankchristopherquispe@gmail.com>
2022-02-07 12:54:00 -0800

    0x01 - UPD(3): 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,39 +12,39 @@ int main(void)
 
 	for (a = 48; a <= 57; a++)
 	{
-		for (b = 48; b <= 57; b++)
+	for (b = 48; b <= 57; b++)
+	{
+		if (a == 57 && b == 57)
+		{}
+		else
 		{
-			if (a == 57 && b == 57)
+			for (c = a; c <= 57; c++)
 			{
-			}
+				for (d = 48; d <= 57; d++)
+				{
+				if (aux != b)
+				{
+					d = b + 1;
+					aux = b;
+					if (b == 57)
+					{
+						d = 48;
+						c++;
+					}
+				}
+				putchar(a);
+				putchar(b);
+				putchar(32);
+				putchar(c);
+				putchar(d);
+
+			if (a == 57 && b == 56 && c == 57 && d == 57)
+			{}
 			else
 			{
-				for (c = a; c <= 57; c++)
-				{
-					for (d = 48; d <= 57; d++)
-					{
-					if (aux == b)
-					{
-					}
-					else
-					{
-						d = b + 1;
-						aux = b;
-					}
-					putchar(a);
-					putchar(b);
-					putchar(32);
-					putchar(c);
-					putchar(d);
-
-				if (a == 57 && b == 56 && c == 57 && d == 57)
-				{
-				}
-				else
-				{
-					putchar(44);
-					putchar(32);
-				}}}}}}
+				putchar(44);
+				putchar(32);
+			}}}}}}
 	putchar(10);
 	return (0);
 }
