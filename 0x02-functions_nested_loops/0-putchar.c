cd29baa862d9a4d33003d35677948ed8fc3072f5
FranQ <frankchristopherquispe@gmail.com>
2022-02-08 14:51:34 -0800

    0x02 - ADD: 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/* filename: main.c */
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar(10);
+	return (0);
+}
