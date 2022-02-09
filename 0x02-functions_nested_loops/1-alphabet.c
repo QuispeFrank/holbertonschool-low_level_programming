d64843a8d0228faf457ca6658c82a00ae059f5d3
FranQ <frankchristopherquispe@gmail.com>
2022-02-08 16:50:32 -0800

    0x02 - ADD: 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - writes the character c to stdout
+ *
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
