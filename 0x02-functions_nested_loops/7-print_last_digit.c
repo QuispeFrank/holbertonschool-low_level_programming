c2aa9d996e3b3195a7296a9a32e6065a9e06fd68
FranQ <frankchristopherquispe@gmail.com>
2022-02-08 20:14:15 -0800

    0x02 - ADD: 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last number of a nuber
+ * @c: parameter to test
+ *
+ * Return: absolute value of a number
+ */
+int print_last_digit(int c)
+{
+	int toreturn;
+	int toprint;
+
+	if (c < 0)
+	{
+		c = c * -1;
+	}
+
+	toprint = c % 10;
+	toreturn = toprint;
+
+	_putchar(toprint + 48);
+	return (toreturn);
+}
