d7cc98006e2d7f89f632c7023fdd4c2dae6a6eae
FranQ <frankchristopherquispe@gmail.com>
2022-02-08 19:27:46 -0800

    0x02 - ADD: 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _print_sign - values if c is < = > than 0
+ * @c: parameter to test
+ *
+ * Return: c is positive: 1, c is zero: 0, c is negative: -1
+ * prints + , 0, - according c value
+ */
+int print_sign(int c)
+{
+	int toreturn;
+	int toprint;
+
+	if (c == 0)
+	{
+		toreturn = 0;
+		toprint = 48;
+	}
+	else
+	{
+		if (c < 0)
+		{
+			toreturn = -1;
+			toprint = 45;
+		}
+		else
+		{
+			toreturn = 1;
+			toprint = 43;
+		}
+	}
+	_putchar(toprint);
+	return (toreturn);
+}
