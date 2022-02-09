25409baf55db71f709a04f2459f9580d0cceda43
FranQ <frankchristopherquispe@gmail.com>
2022-02-08 20:47:34 -0800

    0x02 - ADD: 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - prints the last number of a nuber
+ * @c: parameter to test
+ * @d: parameter to test
+ *
+ * Return: absolute value of a number
+ */
+int add(int c, int d)
+{
+	int toreturn;
+
+	toreturn = c + d;
+	return (toreturn);
+}
