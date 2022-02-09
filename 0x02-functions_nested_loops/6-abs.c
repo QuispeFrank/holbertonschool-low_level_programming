bb35ee772440cf0652de5e69d6d735b200208ca3
FranQ <frankchristopherquispe@gmail.com>
2022-02-08 19:53:01 -0800

    0x02 - ADD: 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of a number
+ * @c: parameter to test
+ *
+ * Return: absolute value of a number
+ */
+int _abs(int c)
+{
+	int toreturn;
+
+	if (c == 0)
+	{
+		toreturn = 0;
+	}
+	else
+	{
+		if (c < 0)
+		{
+			toreturn = -1*c;
+		}
+		else
+		{
+			toreturn = c;
+		}
+	}
+	return (toreturn);
+}
