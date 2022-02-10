5e4f28cf1618b55e8910c080b5a06480ee500de7
FranQ <frankchristopherquispe@gmail.com>
2022-02-10 15:53:12 -0800

    0x04 - ADD: 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Determine if a character is a number
+ * @a: value to test
+ * @b: value to test
+ * Return: ...
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
