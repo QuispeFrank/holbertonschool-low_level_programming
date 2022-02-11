1f8e631f0d6fce1a3c9e86b57f4e147f920e8755
FranQ <frankchristopherquispe@gmail.com>
2022-02-10 18:25:53 -0800

    0x04 - ADD: 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else
+		{
+			if (i % 3 == 0)
+			{
+				printf("%s ", "Fizz");
+			}
+			else
+			{
+				if (i % 5 == 0)
+				{
+					printf("%s ", "Buzz");
+				}
+				else
+				{
+					printf("%d ", i);
+				}
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
+
