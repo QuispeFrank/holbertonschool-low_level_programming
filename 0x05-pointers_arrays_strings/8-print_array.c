4feab40471e8938e025ce088206083891d8ae88e
FranQ <frankchristopherquispe@gmail.com>
2022-02-15 21:51:38 -0800

    0x05 - ADD: 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - function that prints half of a string, followed by a new line.
+ *
+ * @a: pointer to array of integers
+ * @n: number of elements of the array to be printed
+ *
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int n_elements = n;
+	int index = 0;
+
+	while (index < (n_elements - 1))
+	{
+		printf("%d, ", a[index]);
+		index += 1;
+	}
+	printf("%d\n", a[index]);
+}
