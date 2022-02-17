2d0d186ecf9dbd849f8b4f0a00b1595ec13b5e61
FranQ <frankchristopherquispe@gmail.com>
2022-02-17 05:06:41 -0800

    0x06 - UPD: adding protoptype *_strcat(char *dest, char *src)->main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+/**
+ * File: main.h
+ * Auth: Quispe Frank
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
