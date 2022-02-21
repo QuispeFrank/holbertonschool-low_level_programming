a86d3ddce387effb15a6c5c5b61b57521cf4eea5
FranQ <frankchristopherquispe@gmail.com>
2022-02-21 15:31:29 -0800

    0x07 - ADD: 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+/*
+ * File: 1-memcpy.c
+ * Auth: Quispe Frank
+ */
+
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area.
+ *
+ * @dest: A pointer to array of chars where to copy
+ * @src: A pointer to array of chars where from copy
+ * @n: number of chars that will be copied
+ *
+ * Return: @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
