86848c4ad489ba5ae795181a4f768c141735bafc
FranQ <frankchristopherquispe@gmail.com>
2022-02-21 17:47:09 -0800

    0x07 - UPD:FIX null character issue 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,14 +16,17 @@
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
+	unsigned int fin = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			break;
 		i++;
+		if (s[i + 1] == '\0')
+			fin = 1;
 	}
-	if (s[i] == '\0')
+	if (c != '\0' && fin == 1)
 		return (NULL);
 	return (s + i);
 }
