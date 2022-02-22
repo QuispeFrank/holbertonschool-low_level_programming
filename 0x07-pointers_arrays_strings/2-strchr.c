e23d2bc48755f312e4c70edea69e6cea7639abfc
FranQ <frankchristopherquispe@gmail.com>
2022-02-21 16:49:38 -0800

    0x07 - ADD: 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+/*
+ * File: 2-strchr.c
+ * Auth: Quispe Frank
+ */
+
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string.
+ *
+ * @s: A pointer to array of chars where to find the char
+ * @c: char to find
+ * Return: @dest
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			break;
+		i++;
+	}
+	if (s[i] == '\0')
+		return (0);
+	return (s + i);
+}
