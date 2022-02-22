b84f6ac4bc9b8fd6fd0e62d5522d6b07944261e8
FranQ <frankchristopherquispe@gmail.com>
2022-02-21 20:48:59 -0800

    0x07 - ADD: 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,44 @@
+/*
+ * File: 4-strpbrk.c
+ * Auth: Quispe Frank
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - function that gets the length of a prefix substring.
+ *
+ * @s: A pointer to array of chars where to find the chars
+ * @accept: array of chars from it gets the char
+ *
+ * Return: A pointer to the byte in s that matches one of the
+ * bytes in accept, or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int catch = 0, counter = 0;
+	int i = 0, j = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				catch = 1;
+				break;
+			}
+			j += 1;
+		}
+		if (catch == 1)
+			break;
+		i += 1;
+		counter += 1;
+	}
+	if (catch == 1)
+		return (NULL);
+	return (s + counter);
+}
