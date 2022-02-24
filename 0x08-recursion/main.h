b0c8a327833af489c9f9866830d5ec1713cff3cf
FranQ <frankchristopherquispe@gmail.com>
2022-02-24 00:08:16 +0000

    0x08 - ADD: main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+/**
+ * File: main.h
+ * Auth: Quispe Frank
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07pointers_arrays_strings directory.
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
