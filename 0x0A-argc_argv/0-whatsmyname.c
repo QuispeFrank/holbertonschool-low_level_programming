0d0fc1f975200668da346ea224458291a7fad2dd
FranQ <frankchristopherquispe@gmail.com>
2022-02-25 17:14:51 +0000

    0x0A - adding main and parameters description for 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,23 @@
+/*
+ * File: 0-whatsmyame.c
+ * Auth: Quispe Frank *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - funtion that print its own name
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: SUCCESS
+ *
+ */
+
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
-	exit (EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
