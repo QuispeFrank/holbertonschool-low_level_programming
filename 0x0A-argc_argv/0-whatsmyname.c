fd706eef195eae7afcaa31e402f19f3dfa0302a8
FranQ <frankchristopherquispe@gmail.com>
2022-02-25 18:14:56 +0000

    0x0A - ver2.0 of 0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -16,8 +16,8 @@
  *
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", *argv);
 	exit(EXIT_SUCCESS);
 }
