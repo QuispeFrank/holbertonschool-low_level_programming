ad1a1e7dd5b836ee1e244c58c0b8cca6b215b678
FranQ <frankchristopherquispe@gmail.com>
2022-03-01 23:32:16 +0000

    0x0B - Adding 1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+/*
+ * File: 0-create_array.c
+ * Auth: Quispe Frank
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - creates an array of chars, and initializes
+ * it with a specific char.
+ *
+ * @str: argument counter.
+ *
+ * Return: NULL if size = 0
+ * a pointer to the array, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i++])
+
+	p = malloc(sizeof(char) * (i + 1));
+
+	if (str != NULL)
+	{
+		for (i = 0; str[i]; i++)
+			p[i] = str[i];
+		p[i] = '\0';
+	}
+
+	return (p);
+}
