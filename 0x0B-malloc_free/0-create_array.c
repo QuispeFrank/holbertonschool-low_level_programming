18205dae88c0eb9da102d5adb3cb603d1625f943
FranQ <frankchristopherquispe@gmail.com>
2022-03-01 22:19:06 +0000

    0x0B - fixing segmentation fault in 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -24,7 +24,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	p = malloc(sizeof(char) * size);
-	for (i = 0; i < size; i++)
-		p[i] = c;
+	if (p != NULL)
+		for (i = 0; i < size; i++)
+			p[i] = c;
 	return (p);
 }
