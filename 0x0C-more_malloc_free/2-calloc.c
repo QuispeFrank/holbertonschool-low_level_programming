bf03a8ff37045d01703ecea712022552658c186a
FranQ <frankchristopherquispe@gmail.com>
2022-03-03 21:48:48 +0000

    0x0C - adding 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include<stdlib.h>
+
+/**
+ * _calloc - function that concatenates.
+ *
+ * @nmemb: elements of the array.
+ * @size: size of each element in bytes.
+ *
+ * Return: a pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	int lens, i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	lens = nmemb * size;
+	ptr = malloc(lens);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= lens; i++)
+		ptr[i] = '0';
+
+	return (ptr);
+}
