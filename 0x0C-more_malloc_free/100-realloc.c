5f0f4d3bf2beeef45c793ee12076e39807ce85e3
FranQ <frankchristopherquispe@gmail.com>
2022-03-04 07:19:36 +0000

    proof:6 - fixing new_size = 0 && ptr != NULL case in 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
