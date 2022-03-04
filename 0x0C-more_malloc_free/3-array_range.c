e8278ebc21f210de3382ecac408dd36865cc1eef
FranQ <frankchristopherquispe@gmail.com>
2022-03-04 00:00:48 +0000

    0x0C - fixed in 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * (max - min + 1));
+	ptr = malloc(sizeof(int) * size);
 	if (ptr == NULL)
 		return (NULL);
 
