4c474f16aeb70b06d45680bfed3998b732ed5ce4
FranQ <frankchristopherquispe@gmail.com>
2022-03-08 01:36:38 +0000

    dog.h proof 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 /**
  * struct dog - structure
  * @name: char
