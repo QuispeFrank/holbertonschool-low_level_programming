626c0b685524048931e7caddfd3e24c025b0ff18
FranQ <frankchristopherquispe@gmail.com>
2022-03-08 02:56:51 +0000

    adding 2-print_dog.c file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+/**
+ * print_dog - function void
+ *
+ * @d: struct
+ *
+ * Return: nothing.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
