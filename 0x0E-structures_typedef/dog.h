52d83c27e8e7ac81ed201a8dbd75afe6d023eb0c
FranQ <frankchristopherquispe@gmail.com>
2022-03-08 02:05:56 +0000

    adding all prototypes in dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,11 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
