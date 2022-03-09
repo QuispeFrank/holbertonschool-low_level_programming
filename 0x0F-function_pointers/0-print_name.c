022e852dac5b177ab59ce5264b56ff45da3fce72
FranQ <frankchristopherquispe@gmail.com>
2022-03-09 15:42:49 +0000

    checking(0) in 0-print_name.c file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - a function that prints a name.
+ * @name: name to be printed.
+ * @f:pointer to the function that prints.
+ *
+ * Return: nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
