91c3e16225168bccff3981483d8fb2ffcf4ef0d3
FranQ <frankchristopherquispe@gmail.com>
2022-03-10 02:56:01 +0000

    checking(3) in 3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,12 +14,12 @@ int main(int argc __attribute__((unused)), char *argv[])
 	int res, var1, var2;
 	int (*pf)(int, int);
 
-	if (argc > 4)
+	if (argc != 4)
 	{	printf("Error\n");
 		exit(98); }
 	if ((argv[2][0] != '-' && argv[2][0] != '%' && argv[2][0] != '/'
 			&& argv[2][0] != '*' && argv[2][0] != '+' && argv[2][0] != '-')
-			|| argv[2] == NULL)
+			|| argv[2] == NULL || argv[2][1] != '\0')
 	{	printf("Error\n");
 		exit(99);	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
