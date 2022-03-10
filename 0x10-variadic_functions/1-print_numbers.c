9ed66ccd0c38e0ff1e89ba4da412ebc0c041a31a
FranQ <frankchristopherquispe@gmail.com>
2022-03-10 21:21:58 +0000

    checking(0) in print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print_numbers
+ * @separator: separator
+ * @n: n
+ *
+ * Return: int
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list argptr;
+	unsigned int i;
+
+	/* argument validation */
+	if (n == 0)
+	{
+		va_end(argptr);
+		return;
+	}
+
+	/* va_start & va_end */
+	va_start(argptr, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		if (separator == NULL)
+			printf("%d", va_arg(argptr, int));
+		else
+			printf("%d%s", va_arg(argptr, int), separator);
+	}
+	printf("%d\n", va_arg(argptr, int));
+	va_end(argptr);
+}
