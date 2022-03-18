b1b41a0ecf9cb31bf0295c3635e13c9151f705fa
FranQ <frankchristopherquispe@gmail.com>
2022-03-18 00:55:12 +0000

    checking(0) in 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+ * _strlen -  function that returns the length of a string
+ * @s: ...
+ * Return: returns the length of a string
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+
+/**
+ * add_node - function that adds a new node
+ * at the beginning of a list_t list
+ *
+ * @head: head of the linked list
+ * @str: needs to be duplicated
+ * Return: a pointer to the last structure
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	/* creo el nodo */
+	list_t *node = malloc(sizeof(list_t));
+
+	/* validacion */
+	if (str == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	/* asigno valores y apunto al primer nodo */
+	node->str = strdup(str);
+	if (node->str == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->len = _strlen(node->str);
+	node->next = *head;
+
+	/* head apunta al nuevo nodo */
+	*head = node;
+
+	return (node);
+}
