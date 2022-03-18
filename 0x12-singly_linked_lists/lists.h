9f84eff79786ce64c3d066c275162351e8e6ad67
FranQ <frankchristopherquispe@gmail.com>
2022-03-18 00:55:39 +0000

    checking(0) in lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,6 +18,9 @@ typedef struct list_s
 
 	#include <stdio.h>
 	#include <stdlib.h>
+	#include <string.h>
+
+	int _strlen(const char *);
 	size_t print_list(const list_t *h);
 	size_t list_len(const list_t *h);
 	list_t *add_node(list_t **head, const char *str);
