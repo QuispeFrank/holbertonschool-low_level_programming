11dedc5dc0eec540d15b59d56a12379f9488ebee
FranQ <frankchristopherquispe@gmail.com>
2022-03-22 18:14:50 +0000

    checking(0) in 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a list_t list.
+ * @head: A pointer to the list_t list.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
