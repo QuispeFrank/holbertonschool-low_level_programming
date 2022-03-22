ee4a0a7e5a6542084f157366df6984721817073b
FranQ <frankchristopherquispe@gmail.com>
2022-03-22 19:22:05 +0000

    checking(4) in 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if ((*head) == NULL)
+	if (!head)
 		return;
 
 	while (*head)
@@ -19,5 +19,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = tmp;
 	}
-	*head = NULL;
+	head = NULL;
 }
