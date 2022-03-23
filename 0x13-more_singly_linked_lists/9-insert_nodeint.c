bcccf92ca30905321d2eea289b42d1e25089f149
FranQ <frankchristopherquispe@gmail.com>
2022-03-23 00:51:30 +0000

    checking(6) in 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,7 +45,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		ptr = ptr->next;
 	}
-	if (ptr->next == NULL)
+	if (idx == 0 && ptr->next == NULL)
 	{
 		free(node);
 		return (NULL);
@@ -54,4 +54,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node->next = ptr->next;
 	ptr->next = node;
 	return (node);
+
 }
