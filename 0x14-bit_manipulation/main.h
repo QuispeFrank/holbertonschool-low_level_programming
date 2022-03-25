8fc6f0ce8a2c90fe42beb4955cd1c0abf4a85515
FranQ <frankchristopherquispe@gmail.com>
2022-03-25 04:22:36 +0000

    adding get_endianness prototype in main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -12,4 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
+int get_endianness(void);
+
 #endif
