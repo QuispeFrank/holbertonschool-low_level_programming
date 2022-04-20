51c97f68b4d89ca8bb9f6537768950632f26e817
FranQ <frankchristopherquispe@gmail.com>
2022-04-20 17:34:55 +0000

    modified comment in 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
+	/* prints char, int, long int, long long int and float sizes. */
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
