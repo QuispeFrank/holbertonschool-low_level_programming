3b333775c074caa385eb091f4026bd352bca328c
FranQ <frankchristopherquispe@gmail.com>
2022-04-21 04:21:03 +0000

    0-putchar.c refactoring Ok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,11 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar(10);
+	int index;
+	char ascii[] = "_putchar\n";
+
+	for (index = 0; ascii[index]; index++)
+		_putchar(ascii[index]);
+
 	return (0);
 }
