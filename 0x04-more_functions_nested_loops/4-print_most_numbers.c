0b26478f7766e51c561e1af0d707137d28663f16
FranQ <frankchristopherquispe@gmail.com>
2022-04-22 01:19:36 +0000

    4-print_most_numbers.c refactoring Ok

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_most_numbers - Determine if a character is a number
- * Return: ...
+ * print_most_numbers - prints digits 0-9 except 2 & 4.
+ *
+ * Return: nothing.
  */
 void print_most_numbers(void)
 {
 	int digit;
 
-	for (digit = 0; digit <= 9; digit++)
+	/* prints 0 1 3 5 6 7 8 9 */
+	for (digit = '0'; digit <= '9'; digit++)
 	{
-		if (digit != 2 && digit != 4)
-		{
-			_putchar(48 + digit);
-		}
+		if (digit != '2' && digit != '4')
+			_putchar(digit);
 	}
-	_putchar(10);
+	_putchar('\n');
 }
