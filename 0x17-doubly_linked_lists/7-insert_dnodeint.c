248010197e3c36d161946482f434723509a18cfb
Frank <frankchristopher@gmail.com>
2022-05-05 23:13:51 +0000

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -62,7 +62,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/* inicio y final */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	else if (idx == len)
+	if (idx == len)
 		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
