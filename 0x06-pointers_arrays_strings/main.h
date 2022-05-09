5919171f64466169add506484b0c53efae9aadf4
Frank <frankchristopher@gmail.com>
2022-05-09 02:49:58 +0000

    main.h gets _putchar function

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,8 @@
-/**
- * File: main.h
- * Auth: Quispe Frank
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
 
+/* helper */
+int _putchar(char c);
+
+/*****functions*/
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -13,3 +11,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+void print_number(int n);
