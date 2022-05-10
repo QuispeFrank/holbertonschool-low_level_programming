e63052aa0aecc1c10776987311ab2e723d6edfb3
Frank <frankchristopher@gmail.com>
2022-05-10 00:26:34 +0000

    main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,26 +1,26 @@
 #ifndef __SHARED_LIB__
 #define __SHARED_LIB__
 
-/****** functions */
-/* 1 */	int _putchar(char c);
-/* 2 */	int _islower(int c);
-/* 3 */	int _isalpha(int c);
-/* 4 */	int _abs(int n);
-/* 5 */	int _isupper(int c);
-/* 6 */	int _isdigit(int c);
-/* 7 */	int _strlen(char *s);
-/* 8 */	void _puts(char *s);
-/* 9 */	char *_strcpy(char *dest, char *src);
-/*10 */	int _atoi(char *s);
-/*11 */	char *_strcat(char *dest, char *src);
-/*12 */	char *_strncat(char *dest, char *src, int n);
-/*13 */	char *_strncpy(char *dest, char *src, int n);
-/*14 */	int _strcmp(char *s1, char *s2);
-/*15 */	char *_memset(char *s, char b, unsigned int n);
-/*16 */	char *_memcpy(char *dest, char *src, unsigned int n);
-/*17 */	char *_strchr(char *s, char c);
-/*18 */	unsigned int _strspn(char *s, char *accept);
-/*19 */	char *_strpbrk(char *s, char *accept);
-/*20 */	char *_strstr(char *haystack, char *needle);
+/**** functions */
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
