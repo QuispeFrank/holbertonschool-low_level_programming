d3b190aff6a96529718b49323296a6f7c7cb8673
FranQ <frankchristopherquispe@gmail.com>
2022-05-27 05:13:09 +0000

    hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,9 +36,9 @@ typedef struct hash_table_s
 
 
 /********* functions */
-/* 0 */ hash_table_t *hash_table_create(unsigned long int size);
-/* 1 */ unsigned long int hash_djb2(const unsigned char *str);
-/* 2 */ unsigned long int key_index(const unsigned char *key, unsigned long int size);
-/* 3 */ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 #endif
