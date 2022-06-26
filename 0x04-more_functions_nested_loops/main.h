ea67f3fa60498a0fe0d3fbd5e096abf85c78ee1a
FranQ <frankchristopherquispe@gmail.com>
2022-06-26 20:49:14 +0000

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,19 +1,21 @@
 #ifndef __MORE__NESTED__LOOPS__
 #define __MORE__NESTED__LOOPS__
 
-	#include <stdio.h>
+/******* std libraries */
+#include <stdio.h>
 
-	int _isupper(int c);
-	int _isdigit(int c);
-	int mul(int a, int b);
-	void print_numbers(void);
-	void print_most_numbers(void);
-	void more_numbers(void);
-	void print_line(int n);
-	void print_diagonal(int n);
-	void print_square(int size);
-	void print_triangle(int size);
-	int _putchar(char c);
-	void print_number(int n);
+/*** prototypes */
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _putchar(char c);
+void print_number(int n);
 
 #endif
